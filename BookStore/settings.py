028f2d35cda30392b7beb75ca83106ef3b6f603c
RahulMS1798 <msrahul1798@gmail.com>
2024-03-03 01:26:24 +0530

    static config

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -108,6 +109,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
